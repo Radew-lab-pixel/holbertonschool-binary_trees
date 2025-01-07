27eb2188e0b80a3513fa3db0a589fc3169f47fcd
OEM Configuration (temporary user) <ktmqliu@gmail.com>
2025-01-07 15:29:05 +1100

    Amend  0-node.c - failed malloc check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		perror("Error in creating node\n");
 		free(new_node);
-		return (NULL);
+		/* return (NULL);  remove as failed malloc checker */
 	}
 	/**
 	 * if (root == NULL) // root is absent
