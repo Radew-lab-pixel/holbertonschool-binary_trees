ebe42e35f61873859eab549bcd25227bf25f4cd3
OEM Configuration (temporary user) <ktmqliu@gmail.com>
2025-01-09 10:47:42 +1100

    Amend 15-binary_tree_is_full.c - add comments

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -5,6 +5,7 @@
  * binary_tree_is_full - check a node has 0 or 2 childs
  * @tree : pointer to the node
  * Return: 1 if true else 0
+ * Description : using AND gate concept to 
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
@@ -15,13 +16,14 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (current == NULL)
 		return (0); /*false */
 
+	/* if two childs exist */
 	if ((current->left) && (current->right))
 	{	/* return (1); */
 		lefth = binary_tree_is_full(current->left);
 		righth = binary_tree_is_full(current->right);
 		return (lefth && righth); /* And gate */
 	}
-	else if ((!current->left) && (!current->right))
+	else if ((!current->left) && (!current->right)) /* no child */
 		return (1); /*true */
 	else
 		return (0); /*false */
