ed1acdd8d9492e764aa6dcc24e0616930925fae5
OEM Configuration (temporary user) <ktmqliu@gmail.com>
2025-01-09 13:41:46 +1100

    Add 17-binary_tree_sibling.c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling -  finds the sibling of a node
+ * @node : ppinter of the node
+ * Return: node of the sibling
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *current = node;
+	binary_tree_t *parent = NULL;
+	int lefth = 0, righth = 0;
+
+	if (current == NULL)
+		return (NULL);
+
+	parent = current->parent;
+	if (parent == NULL)
+		return (NULL);
+
+	lefth = parent->left->n;
+	righth = parent->right->n;
+
+	if (lefth == current->n) /* current-> is lefth child */
+		return (parent->right);
+	else if (righth == current->n)
+		return (parent->left);
+	else
+		return (NULL);
+
+}
